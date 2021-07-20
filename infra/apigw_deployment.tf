eeb7d055ac6225b1dd0a8d65a80b4ffc32829590
GeorgiStavrev <georgi@insurify.com>
2021-07-20 09:56:09 +0300

    adds dependencies between resources

--- infra/apigw_deployment.tf
+++ infra/apigw_deployment.tf
@@ -35,6 +35,7 @@ resource "aws_api_gateway_stage" "api_stage" {
 
 module "playbook_api_subdomain_mapping" {
   source          = "git@github.com:moggiez/terraform-modules.git//api_subdomain_mapping"
+  depends_on      = [aws_acm_certificate._, aws_api_gateway_stage.api_stage]
   api             = aws_api_gateway_rest_api._
   api_stage_name  = local.stage
   api_subdomain   = "playbooks-api"
